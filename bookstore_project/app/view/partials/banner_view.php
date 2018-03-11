94f1ad9458307f40ecbaf0b2e05f44243f8ad052
Long <vanlong121996@gmail.com>
2018-03-11 22:51:11 +0700

    11/3/2018

--- bookstore_project/app/view/partials/banner_view.php
+++ bookstore_project/app/view/partials/banner_view.php
@@ -1,4 +1,4 @@
-<section id="content-holder" class="container-fluid container">
+<section id="content-holder" class="container-fluid container" style="    box-shadow: 0 0 1px 1px rgba(128,128,128, 1);">
     <section class="row-fluid">
         <section class="span9 first">
             <div class="blog-sec-slider">
